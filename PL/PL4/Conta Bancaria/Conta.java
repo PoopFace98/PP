c7fcb76816062e2a57ebcca6193008c12c98f1ec
PoopFace98 <32722225+PoopFace98@users.noreply.github.com>
2019-02-28 21:58:03 +0000

    sdadas

---
+++ PL/PL4/Conta Bancaria/Conta.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Conta{
+	private String id;
+	private double saldo;
+	private String nome;
+	private int menu;
+	
+	Scanner input = new Scanner(System.in);
+	
+	public Conta(String id, String nome, double saldo){
+		this.id = id;
+		this.saldo = saldo;
+		this.nome = nome;
+	}
+	
+	public String getId(){
+		return id;
+	}
+	
+	public double getSaldo(){
+		return saldo;
+	}
+	
+	public String getNome(){
+		return nome;
+	}
+	
+	public void setSaldo(double saldo){
+		this.saldo = saldo;
+	}
+	
+	public void setId(String id){
+		this.id = id;
+	}
+	
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	
+	public void depositar(double quantia){
+		
+		quantia = input.nextDouble();
+		if(quantia > 0 ){
+		saldo = saldo + quantia;
+		}else{
+		System.out.println("Nao pode depositar quantidades negativas");
+		}
+	}
+	public void levantar(double quantia){
+		quantia = input.nextDouble();
+		if( quantia > 0 && saldo >= quantia){
+		saldo = saldo - quantia;
+		}else{
+		System.out.println("Saldo insuficiente");
+		}
+	}
+	
+	public void consultar(){
+		System.out.println("Numero de conta: " + id);
+		System.out.println("Titular: " + nome);
+		System.out.println("Saldo: " + saldo);
+	}
+
+		
+}
---
+++ PL/PL4/Conta Bancaria/Teste.java
@@ -0,0 +1,24 @@
+public class Teste{
+	public static void main(String[] args){
+		
+		Conta c1 = new Conta("213123", "Ze", 213);
+	
+			c1.consultar();
+
+			System.out.println("Quanto pretende levantar? ");
+			c1.levantar(0);
+
+			System.out.println("Quanto pretende depositar? ");
+			c1.depositar(0);
+
+			c1.consultar();
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
